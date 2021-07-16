e6d0c03d3d834f982fa61fa5a38d0189f3b13759
YanaGuzienko <69dusha69@ukr.net>
2021-07-16 23:31:10 +0300

    done

--- model/index.js
+++ model/index.js
@@ -1,15 +1,147 @@
-// const fs = require('fs/promises')
-// const contacts = require('./contacts.json')
+const Contact = require('./contact');
 
-const listContacts = async () => {}
+const path = require('path');
 
-const getContactById = async (contactId) => {}
+const listContacts = async (req, res) => {
+  try {
+    const response = await Contact.find();
+    res.status(200).json({
+      status: 'success',
+      code: 200,
+      message: 'Success',
+      data: response,
+    });
+    return response;
+  } catch (error) {
+    res.status(404).json({
+      status: 'error',
+      code: 404,
+      message: 'Not Found',
+    });
+  }
+};
 
-const removeContact = async (contactId) => {}
+const getContactById = async (req, res) => {
+  const { contactId } = req.params;
 
-const addContact = async (body) => {}
+  try {
+    const response = await Contact.findOne({ _id: contactId });
 
-const updateContact = async (contactId, body) => {}
+    if (!response) {
+      res.status(404).json({
+        status: 'error',
+        code: 404,
+        message: 'This user does not find',
+      });
+      return;
+    }
+
+    res.status(200).json({
+      status: 'success',
+      code: 200,
+      data: response,
+      message: 'Success',
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: 'error',
+      code: 404,
+      message: 'This user does not find',
+    });
+  }
+};
+
+const removeContact = async (req, res) => {
+  const { contactId } = req.params;
+
+  try {
+    const response = await Contact.findOneAndDelete({ _id: contactId });
+
+    res.json({
+      status: 'success',
+      code: 200,
+      message: 'contact deleted',
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: 'error',
+      code: 404,
+      message: 'Not found',
+    });
+  }
+};
+
+const addContact = async (req, res) => {
+  const { name, email, phone } = req.body;
+
+  try {
+    if (!name || !email || !phone) {
+      res.status(400).json({
+        status: 'error',
+        code: 400,
+        message: 'missing required name field',
+      });
+      return;
+    }
+    const response = await Contact.create(req.body);
+
+    res.status(201).json({
+      status: 'success',
+      code: 201,
+      data: response,
+    });
+  } catch (error) {
+    res.status(404).json({ error: error.message });
+  }
+};
+
+const updateContact = async (req, res) => {
+  const id = req.params;
+
+  try {
+    if (!req.body) {
+      res.status(400).json({
+        status: 'error',
+        code: 400,
+        message: 'missing fields',
+      });
+      return;
+    }
+
+    const newContact = await Contact.findOneAndUpdate({ _id: id.contactId }, { ...req.body }, { new: true });
+    res.status(200).json({
+      status: 'success',
+      cod: 200,
+      data: newContact,
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: 'error',
+      code: 404,
+      message: 'This user does not find',
+    });
+  }
+};
+
+const updateStatusContact = async (req, res) => {
+  const id = req.params;
+  console.log(id);
+
+  try {
+    if (!req.body) {
+      res.status(400).json({ message: 'missing field favorite' });
+    }
+    const newContact = await Contact.findOneAndUpdate({ _id: id.contactId }, { ...req.body }, { new: true });
+
+    res.status(200).json({
+      status: 'success',
+      cod: 200,
+      data: newContact,
+    });
+  } catch (error) {
+    res.status(404).json({ message: 'Not found' });
+  }
+};
 
 module.exports = {
   listContacts,
@@ -17,4 +149,5 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+  updateStatusContact,
+};
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,24 +1,91 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const Joi = require('joi');
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const router = express.Router();
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const {
+  listContacts,
+  getContactById,
+  addContact,
+  updateContact,
+  removeContact,
+  updateStatusContact,
+} = require('../../model/index');
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get('/', listContacts);
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get('/:contactId', getContactById);
 
-router.patch('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete('/:contactId', validateParamsDel, removeContact);
 
-module.exports = router
+router.post('/', validateParamsAdd, addContact);
+
+router.put('/:contactId', validateParamsUpdate, updateContact);
+
+router.patch('/:contactId/favorite', validateParamsFavorite, updateStatusContact);
+
+function validateParamsAdd(req, res, next) {
+  const validateParam = Joi.object({
+    name: Joi.string().required(),
+    email: Joi.string()
+      .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
+      .required(),
+    phone: Joi.number().required(),
+  });
+
+  const validationResult = validateParam.validate(req.body);
+
+  if (validationResult.error) {
+    res.status(400);
+  }
+  next();
+}
+
+function validateParamsDel(req, res, next) {
+  const validateParam = Joi.object({
+    contactId: Joi.string().required(),
+  });
+
+  const validationResult = validateParam.validate(req.params);
+
+  if (validationResult.error) {
+    res.status(400);
+  }
+  next();
+}
+
+function validateParamsUpdate(req, res, next) {
+  const validateParam = Joi.object({
+    contactId: Joi.string().required(),
+  });
+
+  const validateParamBody = Joi.object({
+    name: Joi.string(),
+    email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }),
+    phone: Joi.number(),
+  });
+
+  const validationResult = validateParam.validate(req.params);
+
+  const validationResultBody = validateParamBody.validate(req.body);
+
+  if (validationResultBody.error || validationResult.error) {
+    res.status(400);
+  }
+  next();
+}
+
+function validateParamsFavorite(req, res, next) {
+  const validateParam = Joi.object({
+    favorite: Joi.boolean().required(),
+  });
+
+  const validationResult = validateParam.validate(req.body);
+
+  if (validationResult.error) {
+    res.status(400);
+  }
+  next();
+}
+
+module.exports = router;
